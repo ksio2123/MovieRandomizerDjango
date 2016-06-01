86a6861d60f1869f5249f107e5b77c7cc555ac6a
ddy214 <ddy214@nyu.edu>
2016-05-31 23:07:28 -0400

    added my own changes

--- MovieRandomizerDjango/MovieRandomizer/movies.py
+++ MovieRandomizerDjango/MovieRandomizer/movies.py
@@ -1,9 +1,11 @@
-class Movies():
+class Movie():
 
-    def __init__(self, title, release_date, description, picture_url, director):
+    def __init__(self, title, release_date, description, picture_url):
         '''Movie object constructor'''
         self.title = title
         self.release_date = release_date
         self.description = description
         self.picture_url = picture_url
-        self.director = director
+
+    def __str__(self):
+        return self.title
--- MovieRandomizerDjango/MovieRandomizer/views.py
+++ MovieRandomizerDjango/MovieRandomizer/views.py
@@ -1,4 +1,26 @@
 from django.shortcuts import render
-
+from django.http.response import HttpResponse
+import json, requests
+from movies import Movie
 
 # Create your views here.
+URL = "https://api.themoviedb.org/3/movie/now_playing?api_key="
+
+
+def home(request):
+
+    with open("/Users/derekyu/projects/ENV/MovieRandomizerDjango/MovieRandomizerDjango/MovieRandomizer/key.json") as json_file:
+        data = json.load(json_file)
+
+    API_KEY = data["key"]
+    api_url = URL + API_KEY
+    movies_json = requests.get(api_url).json()
+
+    movies = movies_json["results"]
+    movie_list = []
+    for movie in movies:
+        picture_url = "http://image.tmdb.org/t/p/w500"
+        new_movie = Movie(movie['title'],movie['release_date'], movie['overview'], picture_url+movie["poster_path"])
+        movie_list.append(new_movie)
+    print movie_list[0]
+    return HttpResponse("hello")
